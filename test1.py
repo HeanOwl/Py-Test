75a0fbc185fda93dfac371117828e616e9ae5c5f
HeanOwl <rmfpemvhwl@naver.com>
2021-09-23 17:03:12 +0900

    첫 커밋 별트리

---
+++ test1.py
@@ -0,0 +1,16 @@
+# print ("test")
+# for i in range(2,10):
+# 	for j in range(1,10):
+# 		print(i*j, end = '')
+# 	print('')
+# for i in range(5):
+# 	print ("*"*(i+1), end = (""))
+# 	print ("")
+#########################################
+# for i in range(5):
+# 	print ((" ")*(5-(i+1))+("*")*(i+1), end = (""))
+# 	print ("")
+########################################
+for i in range(5):
+	print (((" ")*(5-(i+1)))+((("*")+(" "))*(i+1))+((" ")*(5-(i+1))), end =(""))
+	print ("")
